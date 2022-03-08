439bca39d3b050ca22f7f3e2d7eae069c8364433
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-08 23:43:01 +0600

    Added window

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-fetch('https://fenglish.ru/wp-content/uploads/2014/06/carnegi.jpg')
-  .then(response => {
+fetch("/article.json")
+  .then((response) => {
     return response.json();
   })
-  .then(articles => {
-    articles.forEach(article => {
-      const article = document.createElement('article');
-      const img = document.createElement('img');
-      const h2 = document.createElement('h2');
-      const h3 = document.createElement('h3');
-      // const span = document.createElement('span');
-      
-      img.src = article.img;
-      h2.textContent = article.h2;
-      h3.textContent = article.h3;
+  .then((articles) => {
+    articles.forEach((article) => {
+      const articles = document.createElement("article");
+      const img = document.createElement("img");
+      const h2 = document.createElement("h2");
+      const h3 = document.createElement("h3");
 
+      img.src = article.image;
+      h2.textContent = article.title;
+      h3.textContent = article.author;
 
+      articles.append(img);
+      articles.append(h2);
+      articles.append(h3);
+
+      document.body.append(articles);
     });
-  })
-
+  });
+const div = document.createElement("div .window");
+document.querySelector("img").addEventListener("mouseover", function () {
+  div.textContent = article.description;
+});
